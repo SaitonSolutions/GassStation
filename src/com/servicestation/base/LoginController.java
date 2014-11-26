8ac25e005536920400fc76aa84c75d1bd5f79922
Lightway <lightway@Lightways-MacBook-Pro.local>
2014-11-26 21:55:49 +0530

    Package declaration modified.

--- src/com/servicestation/base/LoginController.java
+++ src/com/servicestation/base/LoginController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ServiceStation.base;
+package com.servicestation.base;
 
 import java.net.URL;
 import java.util.ResourceBundle;
